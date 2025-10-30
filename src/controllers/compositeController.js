faeb445efff1c012e80cfaaf1d8f1712f0902b42
you1208 <y.watanabe128@gmail.com>
2025-10-30 13:44:53 +0900

    Add Cloudinary config to compositeController

--- src/controllers/compositeController.js
+++ src/controllers/compositeController.js
@@ -3,6 +3,13 @@ const cloudinary = require('cloudinary').v2;
 
 const prisma = new PrismaClient();
 
+// Cloudinary設定
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 // 全スタンプ取得
 exports.getAllStamps = async (req, res) => {
     try {
